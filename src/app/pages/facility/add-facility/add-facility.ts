7d9c0f6e090259952b081d2a23a293b00ef82b63
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-16 10:26:51 +0530

    added facility folder

---
+++ src/app/pages/facility/add-facility/add-facility.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-facility',
+  imports: [],
+  templateUrl: './add-facility.html',
+  styleUrl: './add-facility.css'
+})
+export class AddFacility {
+
+   
+}
