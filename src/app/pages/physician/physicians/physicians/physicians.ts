d2026d45dce62431d911ec052a2c95a1752ba379
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-18 22:51:44 +0530

    feat: add physicians component
    - Create physicians component under physicians folder
    - Add physicians list page with component structure

---
+++ src/app/pages/physician/physicians/physicians/physicians.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-physicians',
+  imports: [],
+  templateUrl: './physicians.html',
+  styleUrl: './physicians.css',
+})
+export class Physicians {
+
+}
